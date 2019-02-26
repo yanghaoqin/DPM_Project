c5a8f752cb9f87f89a2c6cf2efd94be3a0e53ce2
Raymond H. Yang <raymond.yang@mail.mcgill.ca>
2019-02-26 12:00:23 -0500

    ssh reader

---
+++ sshDataReader/src/sshDataReader/sshDR.java
@@ -0,0 +1,20 @@
+package sshDataReader;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.*;
+
+public class sshDR {
+  
+  public static void main(String args[]) throws Exception {
+    
+    // write out file
+    PrintWriter writer = new PrintWriter("data.csv", "UTF-8");
+    
+    // sensor
+    
+  }
+
+}
